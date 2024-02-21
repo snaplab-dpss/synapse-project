e1993f88d71aac67672b52f3c218e68bb6531d02
Francisco Pereira <fchamicapereira@gmail.com>
2024-02-21 17:53:33 -0500

    Send to controller done

---
+++ eval/micro/send_to_controller/send_to_controller.cpp
@@ -0,0 +1,20 @@
+#include <sycon/sycon.h>
+#include <unistd.h>
+
+using namespace sycon;
+
+void sycon::nf_init() {
+  // No state
+}
+
+void sycon::nf_cleanup() {
+  // No state
+}
+
+bool sycon::nf_process(time_ns_t now, byte_t *pkt, uint16_t size) {
+  cpu_hdr_t *cpu_hdr = (cpu_hdr_t *)packet_consume(pkt, sizeof(cpu_hdr_t));
+  cpu_hdr->out_port = SWAP_ENDIAN_16(nf_config.out_dev_port);
+  return true;
+}
+
+int main(int argc, char **argv) { SYNAPSE_CONTROLLER_MAIN(argc, argv) }
