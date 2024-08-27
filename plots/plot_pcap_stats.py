31548534607422421da6cf1220ae90cc2b18e1fc
Francisco Pereira <fchamicapereira@gmail.com>
2024-08-27 16:45:52 -0400

    Regenering pcap stats plots

--- plots/plot_pcap_stats.py
+++ plots/plot_pcap_stats.py
@@ -17,11 +17,10 @@ OUTPUT_FNAME = "pcap_stats"
 REPORTS_DIR = CURRENT_DIR / ".." / "tools" / "pcaps"
 
 reports = [
-    # { "name": "caida_equinix_nyc", "file": "equinix-nyc.dirB.20190117-140000.UTC.anon.json" },
-    # { "name": "caida_chicago", "file": "equinix-chicago.json" },
-    # { "name": "univ1", "file": "univ1.json" },
-    # { "name": "univ2", "file": "univ2.json" },
-    { "name": "univ2", "file": "univ2-350k.json" },
+    { "name": "caida_equinix_nyc", "file": "equinix-nyc.dirB.20190117-140000.UTC.anon.json" },
+    { "name": "caida_chicago", "file": "equinix-chicago.json" },
+    { "name": "univ1", "file": "univ1.json" },
+    { "name": "univ2", "file": "univ2.json" },
 ]
 
 class CDF(Struct):
@@ -59,12 +58,12 @@ def parse_report(file: Path) -> StatsReport:
 
 def plot(name: str, report: StatsReport):
     figures: list[tuple[str, str, CDF]] = [
-        ("pkt_bytes_cdf", "Packet bytes", report.pkt_bytes_cdf),
-        ("pkts_per_flow_cdf", "Packets/flow", report.pkts_per_flow_cdf),
+        # ("pkt_bytes_cdf", "Packet bytes", report.pkt_bytes_cdf),
+        # ("pkts_per_flow_cdf", "Packets/flow", report.pkts_per_flow_cdf),
         ("top_k_flows_cdf", "Top-k flows", report.top_k_flows_cdf),
-        ("top_k_flows_bytes_cdf", "Top-k flows (bytes)", report.top_k_flows_bytes_cdf),
-        ("flow_duration_cdf", "Flow duration (us)", report.flow_duration_us_cdf),
-        ("flow_ipt_cdf", "Flow inter-packet time (us)", report.flow_dts_us_cdf),
+        # ("top_k_flows_bytes_cdf", "Top-k flows (bytes)", report.top_k_flows_bytes_cdf),
+        # ("flow_duration_cdf", "Flow duration (us)", report.flow_duration_us_cdf),
+        # ("flow_ipt_cdf", "Flow inter-packet time (us)", report.flow_dts_us_cdf),
     ]
 
     for fname, xlabel, cdf in figures:
@@ -81,10 +80,17 @@ def plot(name: str, report: StatsReport):
 
         ax.set_xlabel(xlabel)
         ax.set_ylabel("CDF")
-        ax.set_xlim(xmin=0, xmax=max_x)
         ax.set_ylim(ymin=0, ymax=1.0)
-        ax.set_xticks(xticks)
         ax.set_yticks(yticks)
+
+        if "top_k" in fname:
+            ax.set_xscale("log")
+            ax.set_xticks([1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7])
+            ax.set_xlim(xmin=1e0, xmax=1.5e7)
+            # look for max_x in log scale
+        else:
+            ax.set_xlim(xmin=0, xmax=max_x)
+            ax.set_xticks(xticks)
 
         ax.plot([0] + cdf.values, [0] + cdf.probabilities, label=xlabel)
 
