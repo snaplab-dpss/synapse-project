0882327431026e27b989bf6cb70ce573502dd721
Francisco Pereira <fchamicapereira@gmail.com>
2024-09-24 13:48:34 -0400

    Removed unnecessary NFs

--- dpdk-nfs/gallium-proxy/backend.c
+++
@@ -1,9 +0,0 @@
-#include "backend.h"
-
-#ifdef KLEE_VERIFICATION
-struct str_field_descr backend_descrs[] = {
-    {offsetof(struct Backend, ip), sizeof(uint32_t), 0, "src_ip"},
-    {offsetof(struct Backend, port), sizeof(uint16_t), 0, "src_port"},
-};
-struct nested_field_descr backend_nests[] = {};
-#endif  // KLEE_VERIFICATION
--- dpdk-nfs/gallium-proxy/entry.c
+++
@@ -1,8 +0,0 @@
-#include "entry.h"
-
-#ifdef KLEE_VERIFICATION
-struct str_field_descr entry_descrs[] = {
-    {offsetof(struct Entry, port), sizeof(uint16_t), 0, "port"},
-};
-struct nested_field_descr entry_nests[] = {};
-#endif  // KLEE_VERIFICATION
