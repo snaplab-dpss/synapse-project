116c39a47438e4cf27f6c768387330486b13ca98
Francisco Pereira <fchamicapereira@gmail.com>
2024-11-07 22:22:26 -0500

    Updated profiles

--- tools/generate_profiles.sh
+++ tools/generate_profiles.sh
@@ -30,32 +30,6 @@ gen_and_build_profiler() {
     log_and_run NF=$nf-profiler.cpp make -f $TOOLS_DIR/Makefile.dpdk
 }
 
-kvs() {
-    keys=$1
-    churn=$2
-    distribution=$3
-    zipf_param=$4
-
-    pcap="kvs-k$keys-c$churn-$distribution"
-    if [ "$distribution" == "zipf" ]; then
-        pcap="$pcap$zipf_param"
-    fi
-
-    warmup_pcap=$pcap-warmup
-    pcap_dev_0=$pcap
-    report=$pcap
-    
-    log_and_run $SYNTHESIZED_DIR/build/kvs-profiler \
-        $SYNTHESIZED_DIR/$report.json \
-        --warmup 0:$PCAPS_DIR/$warmup_pcap.pcap \
-        0:$PCAPS_DIR/$pcap_dev_0.pcap
-    
-    log_and_run bdd-visualizer \
-        --in $BDDS_DIR/kvs.bdd \
-        --report $SYNTHESIZED_DIR/$report.json \
-        --out $SYNTHESIZED_DIR/$report.dot
-}
-
 fw() {
     flows=$1
     churn=$2
@@ -112,39 +86,65 @@ nat() {
         --out $SYNTHESIZED_DIR/$report.dot
 }
 
-# gen_and_build_profiler fw
+kvs() {
+    keys=$1
+    churn=$2
+    distribution=$3
+    zipf_param=$4
 
-# fw 10000 0 unif 0
-# fw 10000 0 zipf 0.9
-# fw 10000 0 zipf 0.99
-# fw 10000 0 zipf 1.26
+    pcap="kvs-k$keys-c$churn-$distribution"
+    if [ "$distribution" == "zipf" ]; then
+        pcap="$pcap$zipf_param"
+    fi
 
-# fw 10000 1000000 unif 0
-# fw 10000 1000000 zipf 0.9
-# fw 10000 1000000 zipf 0.99
-# fw 10000 1000000 zipf 1.26
+    warmup_pcap=$pcap-warmup
+    pcap_dev_0=$pcap
+    report=$pcap
+    
+    log_and_run $SYNTHESIZED_DIR/build/kvs-profiler \
+        $SYNTHESIZED_DIR/$report.json \
+        --warmup 0:$PCAPS_DIR/$warmup_pcap.pcap \
+        0:$PCAPS_DIR/$pcap_dev_0.pcap
+    
+    log_and_run bdd-visualizer \
+        --in $BDDS_DIR/kvs.bdd \
+        --report $SYNTHESIZED_DIR/$report.json \
+        --out $SYNTHESIZED_DIR/$report.dot
+}
 
-# fw 10000 100000000 unif 0
-# fw 10000 100000000 zipf 0.9
-# fw 10000 100000000 zipf 0.99
-# fw 10000 100000000 zipf 1.26
+gen_and_build_profiler fw
 
-# gen_and_build_profiler nat
+fw 10000 0 unif 0
+fw 10000 0 zipf 0.9
+fw 10000 0 zipf 0.99
+fw 10000 0 zipf 1.26
 
-# nat 10000 0 unif 0
-# nat 10000 0 zipf 0.9
-# nat 10000 0 zipf 0.99
-# nat 10000 0 zipf 1.26
+fw 10000 1000000 unif 0
+fw 10000 1000000 zipf 0.9
+fw 10000 1000000 zipf 0.99
+fw 10000 1000000 zipf 1.26
 
-# nat 10000 1000000 unif 0
-# nat 10000 1000000 zipf 0.9
-# nat 10000 1000000 zipf 0.99
-# nat 10000 1000000 zipf 1.26
+fw 10000 100000000 unif 0
+fw 10000 100000000 zipf 0.9
+fw 10000 100000000 zipf 0.99
+fw 10000 100000000 zipf 1.26
 
-# nat 10000 100000000 unif 0
-# nat 10000 100000000 zipf 0.9
-# nat 10000 100000000 zipf 0.99
-# nat 10000 100000000 zipf 1.26
+gen_and_build_profiler nat
+
+nat 10000 0 unif 0
+nat 10000 0 zipf 0.9
+nat 10000 0 zipf 0.99
+nat 10000 0 zipf 1.26
+
+nat 10000 1000000 unif 0
+nat 10000 1000000 zipf 0.9
+nat 10000 1000000 zipf 0.99
+nat 10000 1000000 zipf 1.26
+
+nat 10000 100000000 unif 0
+nat 10000 100000000 zipf 0.9
+nat 10000 100000000 zipf 0.99
+nat 10000 100000000 zipf 1.26
 
 gen_and_build_profiler kvs
 
@@ -153,14 +153,14 @@ kvs 100000 0 zipf 0.9
 kvs 100000 0 zipf 0.99
 kvs 100000 0 zipf 1.26
 
-kvs 10000 1000000 unif 0
-kvs 10000 1000000 zipf 0.9
-kvs 10000 1000000 zipf 0.99
-kvs 10000 1000000 zipf 1.26
+kvs 100000 1000000 unif 0
+kvs 100000 1000000 zipf 0.9
+kvs 100000 1000000 zipf 0.99
+kvs 100000 1000000 zipf 1.26
 
-kvs 10000 100000000 unif 0
-kvs 10000 100000000 zipf 0.9
-kvs 10000 100000000 zipf 0.99
-kvs 10000 100000000 zipf 1.26
+kvs 100000 100000000 unif 0
+kvs 100000 100000000 zipf 0.9
+kvs 100000 100000000 zipf 0.99
+kvs 100000 100000000 zipf 1.26
 
-# gen_and_build_profiler pol
+gen_and_build_profiler pol
