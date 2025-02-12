b2b2cf4c2ffd625711f3dbf88317bac980100c8a
Francisco Pereira <fchamicapereira@gmail.com>
2025-02-12 15:39:54 +0000

    Moved fwd to tofino folder

--- eval/throughput-simple-forwarder.py
+++ eval/throughput-simple-forwarder.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 from experiments.hosts.pktgen import Pktgen
 from experiments.hosts.tofino_tg import TofinoTG, TofinoTGController
+from experiments.hosts.switch import Switch
 from experiments.hosts.synapse import SynapseController
 
 def main():
@@ -19,24 +20,24 @@ def main():
     with open(args.config_file, "rb") as f:
         config = tomli.load(f)
     
-    # print("Launching Tofino DUT...")
-    # synapse_nf = Switch(
-    #     hostname=config["hosts"]["switch_dut"],
-    #     repo=config["repo"]["switch_dut"],
-    #     sde=config["devices"]["switch_dut"]["sde"],
-    #     tofino_version=config["devices"]["switch_dut"]["tofino_version"],
-    #     log_file=config["logs"]["switch_dut"],
-    # )
+    print("Launching Tofino DUT...")
+    dut_switch = Switch(
+        hostname=config["hosts"]["switch_dut"],
+        repo=config["repo"]["switch_dut"],
+        sde=config["devices"]["switch_dut"]["sde"],
+        tofino_version=config["devices"]["switch_dut"]["tofino_version"],
+        log_file=config["logs"]["switch_dut"],
+    )
 
-    # print("Launching controller for Tofino DUT...")
-    # synapse_controller = SynapseController(
-    #     hostname=config["hosts"]["switch_dut"],
-    #     repo=config["repo"]["switch_dut"],
-    #     sde=config["devices"]["switch_dut"]["sde"],
-    #     ports=config["devices"]["switch_dut"]["ports"],
-    #     tofino_version=config["devices"]["switch_dut"]["tofino_version"],
-    #     log_file=config["logs"]["controller_dut"],
-    # )
+    print("Launching controller for Tofino DUT...")
+    synapse_controller = SynapseController(
+        hostname=config["hosts"]["switch_dut"],
+        repo=config["repo"]["switch_dut"],
+        sde=config["devices"]["switch_dut"]["sde"],
+        ports=config["devices"]["switch_dut"]["ports"],
+        tofino_version=config["devices"]["switch_dut"]["tofino_version"],
+        log_file=config["logs"]["controller_dut"],
+    )
 
     print("Launching Tofino TG...")
     tg_switch = TofinoTG(
@@ -47,6 +48,7 @@ def main():
         log_file=config["logs"]["switch_tg"],
     )
 
+    print("Launching Tofino TG controller...")
     tg_controller = TofinoTGController(
         hostname=config["hosts"]["switch_tg"],
         repo=config["repo"]["switch_tg"],
@@ -54,16 +56,19 @@ def main():
         log_file=config["logs"]["controller_tg"],
     )
 
-    # print("Launching pktgen...")
-    # Pktgen(
-    #     hostname=config["hosts"]["pktgen"],
-    #     repo=config["repo"]["pktgen"],
-    #     rx_pcie_dev=config["devices"]["pktgen"]["rx_dev"],
-    #     tx_pcie_dev=config["devices"]["pktgen"]["tx_dev"],
-    #     nb_tx_cores=config["devices"]["pktgen"]["nb_tx_cores"],
-    #     log_file=config["logs"]["pktgen"],
-    # )
+    print("Launching pktgen...")
+    pktgen = Pktgen(
+        hostname=config["hosts"]["pktgen"],
+        repo=config["repo"]["pktgen"],
+        rx_pcie_dev=config["devices"]["pktgen"]["rx_dev"],
+        tx_pcie_dev=config["devices"]["pktgen"]["tx_dev"],
+        nb_tx_cores=config["devices"]["pktgen"]["nb_tx_cores"],
+        log_file=config["logs"]["pktgen"],
+    )
 
+    dut_switch.install(src_in_repo="tofino/forwarder")
+    synapse_controller.launch(src_in_repo="tofino/forwarder")
+    
     tg_switch.install()
     tg_switch.launch()
     tg_switch.wait_ready()
@@ -72,6 +77,7 @@ def main():
         symmetric=[],
         route=[],
     )
+    tg_switch.kill_switchd()
 
 if __name__ == "__main__":
     main()
