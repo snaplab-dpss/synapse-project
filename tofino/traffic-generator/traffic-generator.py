063895c52f42b7d3331c88cf4e9a6ab46a5e32d4
Francisco Pereira <fchamicapereira@gmail.com>
2025-02-18 12:50:43 +0000

    Minor adjustments

--- tofino/traffic-generator/traffic-generator.py
+++ tofino/traffic-generator/traffic-generator.py
@@ -19,8 +19,8 @@ assert all([ p != TG_PORT for p in DUT_CONNECTED_PORTS ])
 CONFIGURATIONS = {
 	# 32 LANs
 	"nop": {
-		"broadcast": [ p for p in DUT_CONNECTED_PORTS ],
-		"symmetric": [],
+		"broadcast": [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31 ],
+		"symmetric": [ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32 ],
 		"route": [],
 	},
 
