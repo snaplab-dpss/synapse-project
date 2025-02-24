fc49510242d1a2a60537be6aa10a3d5ea49437ef
Francisco Pereira <fchamicapereira@gmail.com>
2025-02-24 20:44:37 +0000

    Fixed z3 build on synapse

--- synapse/cmake/find_z3.cmake
+++ synapse/cmake/find_z3.cmake
@@ -19,9 +19,6 @@ set(ENABLE_Z3 1) # For config.h
 set(HAVE_Z3_GET_ERROR_MSG_NEEDS_CONTEXT 1)
 set (_old_CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES}")
 set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${Z3_LIBRARIES})
-check_prototype_definition(Z3_get_error_msg
-  "Z3_string Z3_get_error_msg(Z3_context c, Z3_error_code err)"
-  "NULL" "${Z3_INCLUDE_DIRS}/z3.h" HAVE_Z3_GET_ERROR_MSG_NEEDS_CONTEXT)
 set(CMAKE_REQUIRED_LIBRARIES ${_old_CMAKE_REQUIRED_LIBRARIES})
 if (HAVE_Z3_GET_ERROR_MSG_NEEDS_CONTEXT)
   message(STATUS "Z3_get_error_msg requires context")
