3a912b2f8422d4a98f6fcfc8987b9e7be8efb7dc
jra <joao.amado@tecnico.ulisboa.pt>
2025-02-25 10:21:30 +0000

    netcache: no_cache_activated arg on the controller side.

--- tofino/netcache/controller/src/main.cpp
+++ tofino/netcache/controller/src/main.cpp
@@ -22,10 +22,12 @@ std::shared_ptr<netcache::ProcessQuery> netcache::ProcessQuery::process_query;
 
 struct args_t {
 	std::string conf_file_path;
+	bool no_cache_activated;
 
-	args_t(int argc, char **argv) {
-		conf_file_path = std::string(argv[argc-1]);
+	args_t(int argc, char **argv) : no_cache_activated(false) {
 		parse_help(argc, argv);
+		parse_conf(argc, argv);
+		parse_no_cache_activated(argc, argv);
 	}
 
 	void help(char **argv) {
@@ -48,64 +50,86 @@ struct args_t {
 		}
 	}
 
-	void dump() const {
-		std::cout << "\n";
-		std::cout << "Configuration:\n";
-		std::cout << "	conf (w/ topo): " << conf_file_path << "\n";
+	void parse_conf(int argc, char **argv) {
+		std::string target = "--";
+
+		for (auto i = 1; i < argc; i++) {
+			auto arg = std::string(argv[i]);
+			if (arg.compare(target) == 0) {
+				conf_file_path = std::string(argv[i+1]);
+				return;
+			}
+		}
+		std::cout << "Error: No conf file path provided." << std::endl;
+		exit(1);
+	}
+
+	void parse_no_cache_activated(int argc, char **argv) {
+		std::string target = "--no_cache_activated";
+
+		for (auto i = 1; i < argc; i++) {
+			auto arg = std::string(argv[i]);
+			if (arg.compare(target) == 0) {
+				no_cache_activated = true;
+				return;
+			}
+		}
 	}
 };
 
-void reset_counters() {
-	auto last_ts_key	= std::chrono::steady_clock::now();
-	auto last_ts_cm		= std::chrono::steady_clock::now();
-	auto last_ts_bloom	= std::chrono::steady_clock::now();
+void reset_counters(bool no_cache_activated) {
+	if (!no_cache_activated) {
+		auto last_ts_key	= std::chrono::steady_clock::now();
+		auto last_ts_cm		= std::chrono::steady_clock::now();
+		auto last_ts_bloom	= std::chrono::steady_clock::now();
 
-	while (!stop_reset_timer) {
-		// Check if the reset timers have elapsed.
-		// If so, reset the respective counters.
-		auto cur_ts = std::chrono::steady_clock::now();
-		auto elapsed_time_key =
-				std::chrono::duration_cast<std::chrono::seconds>(cur_ts-last_ts_key);
-		auto elapsed_time_cm =
-				std::chrono::duration_cast<std::chrono::seconds>(cur_ts-last_ts_cm);
-		auto elapsed_time_bloom =
-				std::chrono::duration_cast<std::chrono::seconds>(cur_ts-last_ts_bloom);
+		while (!stop_reset_timer) {
+			// Check if the reset timers have elapsed.
+			// If so, reset the respective counters.
+			auto cur_ts = std::chrono::steady_clock::now();
+			auto elapsed_time_key =
+					std::chrono::duration_cast<std::chrono::seconds>(cur_ts-last_ts_key);
+			auto elapsed_time_cm =
+					std::chrono::duration_cast<std::chrono::seconds>(cur_ts-last_ts_cm);
+			auto elapsed_time_bloom =
+					std::chrono::duration_cast<std::chrono::seconds>(cur_ts-last_ts_bloom);
 
-		if (elapsed_time_key.count()
-				>= netcache::Controller::controller->conf.key_cntr.reset_timer) {
-			netcache::Controller::controller->reg_key_count.set_all_false();
-			last_ts_key = cur_ts;
-			#ifndef NDEBUG
-			std::cout << "Reset timer: data plane reg_key_count." << std::endl;
-			#endif
-		}
+			if (elapsed_time_key.count()
+					>= netcache::Controller::controller->conf.key_cntr.reset_timer) {
+				netcache::Controller::controller->reg_key_count.set_all_false();
+				last_ts_key = cur_ts;
+				#ifndef NDEBUG
+				std::cout << "Reset timer: data plane reg_key_count." << std::endl;
+				#endif
+			}
 
-		if (elapsed_time_key.count()
-				>= netcache::Controller::controller->conf.cm.reset_timer) {
-			netcache::Controller::controller->reg_cm_0.set_all_false();
-			netcache::Controller::controller->reg_cm_1.set_all_false();
-			netcache::Controller::controller->reg_cm_2.set_all_false();
-			netcache::Controller::controller->reg_cm_3.set_all_false();
-			last_ts_cm = cur_ts;
-			#ifndef NDEBUG
-			std::cout << "Reset timer: data plane reg_cm_0." << std::endl;
-			std::cout << "Reset timer: data plane reg_cm_1." << std::endl;
-			std::cout << "Reset timer: data plane reg_cm_2." << std::endl;
-			std::cout << "Reset timer: data plane reg_cm_3." << std::endl;
-			#endif
-		}
+			if (elapsed_time_key.count()
+					>= netcache::Controller::controller->conf.cm.reset_timer) {
+				netcache::Controller::controller->reg_cm_0.set_all_false();
+				netcache::Controller::controller->reg_cm_1.set_all_false();
+				netcache::Controller::controller->reg_cm_2.set_all_false();
+				netcache::Controller::controller->reg_cm_3.set_all_false();
+				last_ts_cm = cur_ts;
+				#ifndef NDEBUG
+				std::cout << "Reset timer: data plane reg_cm_0." << std::endl;
+				std::cout << "Reset timer: data plane reg_cm_1." << std::endl;
+				std::cout << "Reset timer: data plane reg_cm_2." << std::endl;
+				std::cout << "Reset timer: data plane reg_cm_3." << std::endl;
+				#endif
+			}
 
-		if (elapsed_time_bloom.count()
-				>= netcache::Controller::controller->conf.bloom.reset_timer) {
-			netcache::Controller::controller->reg_bloom_0.set_all_false();
-			netcache::Controller::controller->reg_bloom_1.set_all_false();
-			netcache::Controller::controller->reg_bloom_2.set_all_false();
-			last_ts_bloom = cur_ts;
-			#ifndef NDEBUG
-			std::cout << "Reset timer: data plane reg_bloom_0." << std::endl;
-			std::cout << "Reset timer: data plane reg_bloom_1." << std::endl;
-			std::cout << "Reset timer: data plane reg_bloom_2." << std::endl;
-			#endif
+			if (elapsed_time_bloom.count()
+					>= netcache::Controller::controller->conf.bloom.reset_timer) {
+				netcache::Controller::controller->reg_bloom_0.set_all_false();
+				netcache::Controller::controller->reg_bloom_1.set_all_false();
+				netcache::Controller::controller->reg_bloom_2.set_all_false();
+				last_ts_bloom = cur_ts;
+				#ifndef NDEBUG
+				std::cout << "Reset timer: data plane reg_bloom_0." << std::endl;
+				std::cout << "Reset timer: data plane reg_bloom_1." << std::endl;
+				std::cout << "Reset timer: data plane reg_bloom_2." << std::endl;
+				#endif
+			}
 		}
 	}
 }
@@ -116,7 +140,6 @@ void reset_counters() {
 
 int main(int argc, char **argv) {
 	args_t args(argc, argv);
-	args.dump();
 
 	netcache::conf_t conf = netcache::parse_conf_file(args.conf_file_path);
 
@@ -130,7 +153,7 @@ int main(int argc, char **argv) {
 	auto instance = new netcache::ProcessQuery();;
 	netcache::ProcessQuery::process_query = std::shared_ptr<netcache::ProcessQuery>(instance);
 
-	std::thread reset_thread(reset_counters);
+	std::thread reset_thread(reset_counters, args.no_cache_activated);
 	// std::thread query_thread(query_listener, std::ref(process_query));
 
 	std::cerr << "\nNetCache controller is ready.\n";
