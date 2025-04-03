69f0484a6acbef30ab1cde4ec922c454201a6f58
Francisco Pereira <fchamicapereira@gmail.com>
2025-04-03 09:17:35 +0000

    New script for building synapse nfs

---
+++ tofino/tools/build_synapse_nf.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+TOFINO_MAKEFILE="$SCRIPT_DIR/Makefile"
+LIBSYCON="$SCRIPT_DIR/../../libsycon"
+
+if [[ -z "${SDE_INSTALL}" ]]; then
+	echo "SDE_INSTALL env var not set."
+	exit 1
+fi
+
+nf=$1
+
+if [ -z "$nf" ]; then
+	echo "Usage: $0 <nf>"
+	exit 1
+fi
+
+app="${nf%.*}"
+p4_program=$app.p4
+cpp_program=$app.cpp
+
+if [ ! -f "$p4_program" ]; then
+	echo "File not found: $p4_program"
+	exit 1
+fi
+
+if [ ! -f "$cpp_program" ]; then
+	echo "File not found: $cpp_program"
+	exit 1
+fi
+
+APP=$app make -f $TOFINO_MAKEFILE install-tofino2
+
+$LIBSYCON/install.sh
+APP=$app make -f $TOFINO_MAKEFILE clean
+APP=$app make -f $TOFINO_MAKEFILE controller -j
+
