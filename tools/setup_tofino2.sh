a470088efbce29186a1b6888fb318e6e23e2b055
Francisco Pereira <fchamicapereira@gmail.com>
2025-06-04 10:50:46 +0000

    OK NOW IT'S FIXED FOR REAL

--- tools/setup_tofino2.sh
+++ tools/setup_tofino2.sh
@@ -5,4 +5,4 @@ set -euox pipefail
 sudo ifconfig enx020000000002 up
 sudo $SDE_INSTALL/bin/bf_kpkt_mod_unload $SDE_INSTALL || true
 sudo $SDE_INSTALL/bin/bf_kdrv_mod_load $SDE_INSTALL || true
-sudo $SDE_INSTALL/bin/bf_fpga_mod_load $SDE_INSTAL || true
+sudo $SDE_INSTALL/bin/bf_fpga_mod_load $SDE_INSTALL || true
