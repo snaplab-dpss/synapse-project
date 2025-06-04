91d7b69d55f9760fb9dfc60cd08dd6edb2874fd3
Francisco Pereira <fchamicapereira@gmail.com>
2025-06-04 10:22:01 +0000

    Small update to the setup script for geodude

--- tools/setup_geodude.sh
+++ tools/setup_geodude.sh
@@ -8,5 +8,5 @@ pushd /opt/dpdk-kmods/linux/igb_uio
     sudo insmod igb_uio.ko
 popd
 
-sudo dpdk-devbind.py -b igb_uio 0000:af:00.1
+sudo dpdk-devbind.py -b igb_uio 0000:af:00.0 0000:af:00.1
 sudo dpdk-hugepages.py --setup 20G
