0140402882c5701f9e3387ad5b85d779062522b6
Francisco Pereira <fchamicapereira@gmail.com>
2025-07-17 09:55:08 +0000

    Network consolidator done

--- synapse/tools/network-consolidator.cpp
+++ synapse/tools/network-consolidator.cpp
@@ -12,8 +12,14 @@ int main(int argc, char **argv) {
   CLI::App app{"Network consolidator"};
 
   std::filesystem::path input_network_consolidation_plan_file;
+  std::filesystem::path output_consolidated_bdd_file;
+  bool show_plan   = false;
+  bool consolidate = false;
 
   app.add_option("--plan", input_network_consolidation_plan_file, "Plan for network consolidation.")->required();
+  app.add_flag("--show-plan", show_plan, "Show the network consolidation plan.");
+  app.add_flag("--consolidate", consolidate, "Consolidate the network.");
+  app.add_option("--out", output_consolidated_bdd_file, "Output file for the consolidated BDD.");
 
   CLI11_PARSE(app, argc, argv);
 
@@ -21,20 +27,22 @@ int main(int argc, char **argv) {
   Network network = Network::parse(input_network_consolidation_plan_file, &manager);
   network.debug();
 
-  ClusterViz clusterviz = network.build_clusterviz();
-  clusterviz.show(false);
-  std::cout << "Network consolidation plan graph written to: " << clusterviz.get_file_path() << std::endl;
-
-  const BDD bdd = network.consolidate();
-
-  const BDD::inspection_report_t report = bdd.inspect();
-  if (report.status != BDD::InspectionStatus::Ok) {
-    std::cout << "BDD inspection failed: " << report.message << "\n";
-    return 1;
+  if (show_plan) {
+    ClusterViz clusterviz = network.build_clusterviz();
+    clusterviz.show(false);
+    std::cout << "Network consolidation plan graph written to: " << clusterviz.get_file_path() << std::endl;
   }
 
-  std::cout << "BDD inspection passed.\n";
-  BDDViz::visualize(&bdd, false);
+  if (consolidate) {
+    const BDD bdd                         = network.consolidate();
+    const BDD::inspection_report_t report = bdd.inspect();
+    assert_or_panic(report.status == BDD::InspectionStatus::Ok, "BDD inspection failed: %s", report.message.c_str());
+    std::cout << "BDD inspection passed.\n";
+    BDDViz::visualize(&bdd, false);
+    if (!output_consolidated_bdd_file.empty()) {
+      bdd.serialize(output_consolidated_bdd_file);
+    }
+  }
 
   return 0;
 }
