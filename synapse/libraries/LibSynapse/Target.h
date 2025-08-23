f25361bbf3d901acbb9ad7626c8eba109acc4229
Pedro Mateus <prmateus4@gmail.com>
2025-08-23 21:49:52 +0100

    Changed TargetType and ModuleType structures

--- synapse/libraries/LibSynapse/Target.h
+++ synapse/libraries/LibSynapse/Target.h
@@ -10,7 +10,15 @@ namespace LibSynapse {
 class ModuleFactory;
 class TargetContext;
 
-enum class TargetType { x86, Tofino, Controller };
+enum class TargetArchitecture { x86, Tofino, Controller };
+
+struct TargetType {
+  TargetArchitecture type;
+  std::string instance_id;
+
+  TargetType(TargetArchitecture _type, const std::string &_instance_id) : type(_type), instance_id(_instance_id) {}
+  bool operator==(const TargetType &other) const { return type == other.type && instance_id == other.instance_id; }
+};
 
 std::ostream &operator<<(std::ostream &os, TargetType target);
 std::string to_string(TargetType target);
@@ -66,3 +74,11 @@ struct Targets {
 };
 
 } // namespace LibSynapse
+
+namespace std {
+template <> struct hash<LibSynapse::TargetType> {
+  std::size_t operator()(const LibSynapse::TargetType &tt) const {
+    return std::hash<int>()(static_cast<int>(tt.type)) ^ std::hash<std::string>()(tt.instance_id);
+  }
+};
+} // namespace std
