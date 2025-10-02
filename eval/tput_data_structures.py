fb64c86a986f880f24a441c29f478a7f90131ed3
Francisco Pereira <fchamicapereira@gmail.com>
2025-10-02 07:27:24 +0000

    Bug fix on fcfs experiments: was not actually running with different cache values

--- eval/tput_data_structures.py
+++ eval/tput_data_structures.py
@@ -30,7 +30,7 @@ ZIPF_PARAMS = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2]
 # CHURN_FPM = [0]
 # ZIPF_PARAMS = [1.2]
 
-ITERATIONS = 10
+ITERATIONS = 1
 
 
 @dataclass
@@ -66,10 +66,10 @@ NFS = [
             description=f"FCFSCachedTable (cache={cache_capacity})",
             data_out=Path(f"tput_fcfs_cached_table_{cache_capacity}.csv"),
             tofino=Path(f"tofino/data_structures/fcfs_cached_table/fcfs_cached_table_{cache_capacity}.p4"),
-            controller=Path("tofino/data_structures/fcfs_cached_table/fcfs_cached_table.cpp"),
+            controller=Path(f"tofino/data_structures/fcfs_cached_table/fcfs_cached_table_{cache_capacity}.cpp"),
             kvs_mode=False,
         )
-        for cache_capacity in [256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
+        for cache_capacity in [64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
     ],
 ]
 
