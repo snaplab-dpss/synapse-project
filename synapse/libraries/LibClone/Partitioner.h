d28276c8607c8f93161e8cb13799679276771743
Pedro Mateus <prmateus4@gmail.com>
2026-03-17 14:01:30 +0000

    File Rename and config changes

---
+++ synapse/libraries/LibClone/Partitioner.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <LibCore/Debug.h>
+#include <LibCore/Symbol.h>
+#include <LibCore/Types.h>
+
+#include <LibBDD/BDD.h>
+#include <LibBDD/Nodes/Node.h>
+
+#include <LibClone/EmbeddingSolver.h>
+#include <LibClone/PhysicalNetwork.h>
+
+namespace LibClone {
+
+using LibCore::Symbols;
+
+using LibBDD::BDD;
+using LibBDD::bdd_node_id_t;
+using LibBDD::bdd_node_ids_t;
+using LibBDD::BDDNode;
+using LibBDD::Branch;
+
+using LibClone::EmbeddingSolution;
+
+struct target_roots_t {
+  bdd_node_ids_t port_roots;
+  bdd_node_ids_t target_roots;
+};
+
+class NetworkPartitioner {
+private:
+  std::shared_ptr<const BDD> bdd;
+  const PhysicalNetwork &phys_net;
+  const EmbeddingSolution &solution;
+
+public:
+  NetworkPartitioner(const BDD &_bdd, const PhysicalNetwork &_phys_net, const EmbeddingSolution &_solution);
+
+  NetworkPartitioner(const NetworkPartitioner &)            = delete;
+  NetworkPartitioner &operator=(const NetworkPartitioner &) = delete;
+
+  const BDD *get_bdd() const { return bdd.get(); }
+  const PhysicalNetwork &get_physical_network() const { return phys_net; }
+
+  std::unordered_map<DeviceId, std::unique_ptr<const BDD>> process();
+
+  void debug() const;
+};
+} // namespace LibClone
