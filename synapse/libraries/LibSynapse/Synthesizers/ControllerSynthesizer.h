6cef163fc050873fb575d25f9f2e9d6e354106d3
Francisco Pereira <fchamicapereira@gmail.com>
2026-09-10 03:43:22 +0000

    List only the vector table backing tables the data plane declares.
    A vector table holds one table per read site, but a site the search placed on the
    controller has none in the P4 and libsycon aborts binding it; those names were also
    emitted once per branch that considered them, and VectorTable sums value_size
    across the list.

--- synapse/libraries/LibSynapse/Synthesizers/ControllerSynthesizer.h
+++ synapse/libraries/LibSynapse/Synthesizers/ControllerSynthesizer.h
@@ -296,7 +296,7 @@ private:
   void transpile_map_table_decl(const Tofino::MapTable *map_table);
   void transpile_map_set_table_decl(const Tofino::MapSetTable *map_set_table);
   void transpile_guarded_map_table_decl(const Tofino::GuardedMapTable *guarded_map_table);
-  void transpile_vector_table_decl(const Tofino::VectorTable *vector_table);
+  void transpile_vector_table_decl(const EP *ep, const Tofino::VectorTable *vector_table);
   void transpile_dchain_table_decl(const Tofino::DchainTable *dchain_table, time_ns_t expiration_time);
   void transpile_vector_register_decl(const Tofino::VectorRegister *vector_register);
   void transpile_hh_table_decl(const Tofino::HHTable *hh_table, time_ns_t expiration_time);
